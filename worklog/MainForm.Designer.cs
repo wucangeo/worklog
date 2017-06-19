bbcc9d35da9b8fec66970d834b95e1d57d1478b4
wucan <wucan@geo-compass.com>
2017-06-19 19:45:08 +0800

    1.添加任务栏图标；2.添加任务栏图标右键菜单；3.添加开机自启动

---
+++ worklog/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace worklog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmi_show = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_hide = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmi_auto = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmi_exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_show,
+            this.tsmi_hide,
+            this.toolStripSeparator2,
+            this.tsmi_auto,
+            this.toolStripSeparator1,
+            this.tsmi_exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 104);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // tsmi_show
+            // 
+            this.tsmi_show.Name = "tsmi_show";
+            this.tsmi_show.Size = new System.Drawing.Size(152, 22);
+            this.tsmi_show.Text = "显示";
+            // 
+            // tsmi_hide
+            // 
+            this.tsmi_hide.Name = "tsmi_hide";
+            this.tsmi_hide.Size = new System.Drawing.Size(152, 22);
+            this.tsmi_hide.Text = "隐藏";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // tsmi_auto
+            // 
+            this.tsmi_auto.CheckOnClick = true;
+            this.tsmi_auto.Name = "tsmi_auto";
+            this.tsmi_auto.Size = new System.Drawing.Size(152, 22);
+            this.tsmi_auto.Text = "开机启动";
+            this.tsmi_auto.Click += new System.EventHandler(this.tsmi_auto_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // tsmi_exit
+            // 
+            this.tsmi_exit.Name = "tsmi_exit";
+            this.tsmi_exit.Size = new System.Drawing.Size(152, 22);
+            this.tsmi_exit.Text = "退出";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "工作日志";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_show;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_hide;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_exit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_auto;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ worklog/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+namespace worklog
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Name)
+            {
+                case "tsmi_show":
+                    this.Visible = true;
+                    this.WindowState = FormWindowState.Normal;
+                    break;
+                case "tsmi_hide":
+                    this.Visible = false;
+                    break;
+                case "tsmi_exit":
+                    this.Close();
+                    this.Dispose();
+                    break;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Visible = false;
+            e.Cancel = true;
+            return;
+        }
+
+        private void notifyIcon1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Visible = true;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void tsmi_auto_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem tsmi = sender as ToolStripMenuItem;
+            if (tsmi != null)
+            {
+                if (tsmi.Checked)
+                {
+                    //获取程序执行路径..
+                    string starupPath = Application.ExecutablePath;
+                    //class Micosoft.Win32.RegistryKey. 表示Window注册表中项级节点,此类是注册表装.
+                    RegistryKey loca = Registry.LocalMachine;
+                    RegistryKey run = loca.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                    try
+                    {
+                        //SetValue:存储值的名称
+                        run.SetValue("worklog", starupPath);
+                        MessageBox.Show("已启用开机运行!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        loca.Close();
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show(ee.Message.ToString(), "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+                else
+                {
+                    // MessageBox.Show("没有选中");
+                    //获取程序执行路径..
+                    string starupPath = Application.ExecutablePath;
+                    //class Micosoft.Win32.RegistryKey. 表示Window注册表中项级节点,此类是注册表装.
+                    RegistryKey loca = Registry.LocalMachine;
+                    RegistryKey run = loca.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                    try
+                    {
+                        //SetValue:存储值的名称
+                        run.DeleteValue("worklog");
+                        MessageBox.Show("已停止开机运行!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        loca.Close();
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show(ee.Message.ToString(), "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
